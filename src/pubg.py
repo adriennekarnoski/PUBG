a40f25e458d7fcf93a065ad1390fb2a4e71198a6
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2018-11-06 19:19:33 -0800

    api call works and builds dictionary

--- src/pubg.py
+++ src/pubg.py
@@ -3,20 +3,27 @@ import requests
 import os
 from practice_data import data
 from statistics import mean
+import json
 
-# url = "https://api.pubg.com/shards/xbox-na/matches/50e82100-bbfb-4499-be93-a8af7cb6d63c"
+
+# Use to get a random list of matches
 # url = "https://api.pubg.com/shards/xbox-na/samples"
+
+# Use to get information about a specific match
+url = "https://api.pubg.com/shards/xbox-na/matches/17d001b7-bddb-489c-8001-d7228578128f"
+
+#Use to find my matches
 # url = "https://api.pubg.com/shards/xbox-na/players?filter[playerNames]=ehhhdrienne"
 
-# api_key = os.environ.get('API_KEY')
+api_key = os.environ.get('API_KEY')
 
-# header = {
-#   "Authorization": "Bearer " + api_key,
-#   "Accept": "application/vnd.api+json"
-# }
+header = {
+  "Authorization": "Bearer " + api_key,
+  "Accept": "application/vnd.api+json"
+}
 
-# r = requests.get(url, headers=header)
-# response_dict = r.json()
+r = requests.get(url, headers=header)
+response_dict = r.json()
 
 
 player_stats_dict = {
@@ -27,6 +34,7 @@ player_stats_dict = {
     'kill_points': [],
     'kills': [],
     'longest_kill': [],
+    'name': []
 }
 
 
@@ -45,11 +53,11 @@ def filter_player_data(input):
             player_stats_dict['kill_points'].append(stats['killPoints'])
             player_stats_dict['kills'].append(stats['kills'])
             player_stats_dict['longest_kill'].append(stats['longestKill'])
-    print(mean(player_stats_dict['kills']))
-    # find_average(average_dict, count)
+            player_stats_dict['name'].append(stats['name'])
+    # print_average(player_stats_dict)
 
 
-def find_average(input_dict, count):
-    """Find the average of dictionary values."""
-    for key in input_dict:
-        input_dict[key] = input_dict[key] / count
+# def print_average(input_dict):
+#     """Print the average of dictionary values."""
+#     for k, v in input_dict.items():
+#         print("Average {}: {}".format(k, mean(v)))
